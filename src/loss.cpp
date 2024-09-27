b98fc51024d6cf532662eed892a0d2aa1c7f50a0
Martin Drazkovec <536686@mail.muni.cz>
2024-09-27 17:44:20 +0200

    add cross entropy loss f

--- src/loss.cpp
+++ src/loss.cpp
@@ -1,1 +1,10 @@
 #include "loss.h"
+#include <cmath>
+
+double cross_entropy(std::vector<float> predictions, int target) {
+    float loss;
+    for (int i = 0; i < predictions.size(); i++) {
+        loss += target == i ? -log(predictions[i]) : -log(1 - predictions[i]);
+    }
+    return loss;
+}
